97fc4a39a52642eb3d305ceee2d5ec768f05cfff
Danny Paez <dpaez16@yahoo.com>
2025-11-21 15:46:45 -0800

    Reverted file extension renaming

--- Docker/dev/postgres/bootstrap.sh
+++ Docker/dev/postgres/bootstrap.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f /docker-entrypoint-initdb.d/tables.sql
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f /docker-entrypoint-initdb.d/data.sql
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f /docker-entrypoint-initdb.d/tables.ddl
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f /docker-entrypoint-initdb.d/data.ddl
